afe6528aaa38f1a77219afa1d7188276624e29df
Dawid Dylus <36481644+DawidDylus@users.noreply.github.com>
2021-05-17 14:06:12 +0200

    Adding EF, Creating Model
    Co-Authored-By: Adkins1 <14940128+Adkins1@users.noreply.github.com>

---
+++ Notes.DB/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notes.DB
+{
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Server=(localdb)\mssqllocaldb;Database=Notes;Integrated Security=True");
+        }
+    }
+}
+
+
